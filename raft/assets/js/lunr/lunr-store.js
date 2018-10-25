8e10d56fff32a248a7b7facda31956b0f7e78883
shane <shane@buscherhood.com>
2018-10-24 22:46:22 -0500

    bt epic report draft

---
+++ raft/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "I Won the Lottery",
+        "excerpt":"I did have to share the lottery win with 119 others. You see, there were even more deranged individuals besides myself who entered the chance to race 340 miles of rural Iowa gravel roads. In April when the weather is entirely unpredictable. Within 34 hours. Not knowing the route ahead...","categories": ["gravel"],
+        "tags": ["iowa-wind-and-rock"],
+        "url": "http://localhost:4000/gravel/i-won-the-lottery/",
+        "teaser":null},{
+        "title": "Training Commences",
+        "excerpt":"Training actually commenced 3 weeks ago, a few days after official notification that I was in for the first annual Iowa Wind and Rock. There’s no magic formula on training for a 340 mile gravel race. My goal was just to get started with some base training as my riding...","categories": ["training"],
+        "tags": ["iowa-wind-and-rock"],
+        "url": "http://localhost:4000/training/training-commences/",
+        "teaser":null}]
