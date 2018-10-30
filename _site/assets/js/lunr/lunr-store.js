4bea1879bac87503aa6c4924fdbc2aa21990112c
shane <shane@buscherhood.com>
2018-10-29 21:48:21 -0500

    2018 bt epic post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,23 +4,13 @@ var store = [{
         "tags": ["iowa-wind-and-rock"],
         "url": "http://localhost:4000/gravel/i-won-the-lottery/",
         "teaser":null},{
-        "title": "Gear",
-        "excerpt":"Clothing             Item      Choice      Comments                  Shoes      TBD                     Gloves                    Food and Hydration   Caffeine tablets  IbuprofenBike Gear   Tube(s)  C02  Pump  Tire levers  Patch kit  Chain breaker tool  Multi tool  Head lamp  Bike lamp  Battery charger/cable  Elemnt Bolt  Queue card holder  Lube","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/gear/",
-        "teaser":null},{
         "title": "Training Commences",
         "excerpt":"Training actually commenced 3 weeks ago, a few days after official notification that I was in for the first annual Iowa Wind and Rock. There’s no magic formula on training for a 340 mile gravel race. My goal was just to get started with some base training as my riding...","categories": ["training"],
         "tags": ["iowa-wind-and-rock"],
         "url": "http://localhost:4000/training/training-commences/",
         "teaser":null},{
-        "title": "Training Commences",
-        "excerpt":"Training actually commenced 3 weeks ago, a few days after official notification that I was in for the first annual Iowa Wind and Rock. There’s no magic formula on training for a 340 mile gravel race. My goal was just to get started with some base training since my riding...","categories": ["training"],
-        "tags": ["iowa-wind-and-rock"],
-        "url": "http://localhost:4000/training/calendar/",
-        "teaser":null},{
         "title": "2018 BT Epic",
-        "excerpt":"Strava Selection 2018 2016 2018 2016 Highlights Thurs Departed with 4 The mtblimo was roomy, but not everyone could come Drove to Independence MO Thurs Hotel was loud and remote issues Bugs galore Arcade Fire tributeFriday Denny’s Grand Slam Bass Resort arrival midday, checkin, names please Brr it’s cold in...","categories": ["mtb"],
-        "tags": ["race"],
-        "url": "http://localhost:4000/mtb/bt-epic-report/",
+        "excerpt":"I was first introduced to the BT Epic in 2016 and missed it the following year. The BT refers to the Berryman Trail located in Mark Twain National Forest. At one point in time it was designated as an IMBA Epic trail. The race course is roughly 50 miles of...","categories": ["mtb"],
+        "tags": ["race","btepic"],
+        "url": "http://localhost:4000/mtb/bt-epic-race-report/",
         "teaser":null}]
